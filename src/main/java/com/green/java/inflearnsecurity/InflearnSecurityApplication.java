03e7a9ed7093324c131b7e4a869d56f407b55fa7
wltngus200 <wltngus200@gmail.com>
2024-10-28 21:42:26 +0900

    1028 시큐리티 세팅

---
+++ src/main/java/com/green/java/inflearnsecurity/InflearnSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.green.java.inflearnsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/java/inflearnsecurity/InflearnSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.java.inflearnsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
